dc839d9c7ca6e3c054d3457e3a4a2b5a831cbfdb
mynameisvinn <vin.tang@gmail.com>
2018-09-09 19:19:30 -0400

    better error handling

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ treat s3 the same way youd treat sqlite. put/get python objects in s3.
 warning: this is a proof of concept and should not be used in production.
 
 ## example
-we want to save `vin`, an object.
+we want to save `v`, an object.
 ```python
 class Person():
     def __init__(self, name):
@@ -15,11 +15,12 @@ v = Person("vin")
 to put an object, do
 ```python
 from pyswiss import PySwiss
-client = PySwiss()
-client.put(v, "mynameisvinn", "obj")  # use your own bucket/keys
+
+client = PySwiss(bucket="mynameisvinn-111")
+client.put(v, "some_key")
 ```
 to get an object, do
 ```python
-my_object = client.get("mynameisvinn", "obj")
+my_object = client.get("some_key")
 print(my_object.name)  # prints "vin"
 ```
--- pyswiss/PySwiss.py
+++ pyswiss/PySwiss.py
@@ -2,16 +2,26 @@ import pickle
 import boto3
 
 class PySwiss(object):
-    def __init__(self):
+    def __init__(self, bucket):
         self.client = boto3.client('s3')
+        self.bucket = bucket
+        self._check_bucket()
+
+    def _check_bucket(self):
+        """check if bucket exists in s3.
+        """
+        response = self.client.list_buckets()
+        buckets = [bucket['Name'] for bucket in response['Buckets']]
+        if self.bucket not in buckets:
+            raise ValueError("Bucket does not exist")
     
-    def put(self, obj, bucket, key):
+    def put(self, obj, key):
         pickled_obj = pickle.dumps(obj)
-        self.client.put_object(Body=pickled_obj, Bucket=bucket, Key=key)
+        self.client.put_object(Body=pickled_obj, Bucket=self.bucket, Key=key)
         
-    def get(self, bucket, key):
+    def get(self, key):
         try:
-            obj = self.client.get_object(Bucket=bucket, Key=key)
+            obj = self.client.get_object(Bucket=self.bucket, Key=key)
             new_pickled_obj = obj['Body'].read()
             return pickle.loads(new_pickled_obj)
         except Exception as e:
